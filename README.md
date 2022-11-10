7ad10ef99d03b4b16da921acf42f00243f54fe69
Martin Dziura <5863649+mondbaron@users.noreply.github.com>
2022-11-10 18:17:15 +0100

    inital commit
    add an initial version of the install script and documentation

---
+++ README.md
@@ -0,0 +1,23 @@
+## MDZSH Installation Skript
+
+A single-shot installation skript to populate a powerful ZSH configuration on Debian based systems.
+
+The script configures/installs:
+- ZSH (`zsh`) and prerequisites (`curl`, `git`, `fonts-powerline`) via `apt`
+- [Oh My Zsh](https://ohmyz.sh) managing framework via `curl`
+- Plugins:
+    - git
+    - python 
+    - pip 
+    - virtualenv
+    - [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions) via `git`
+    - [zsh-completions](https://github.com/zsh-users/zsh-completions) via `git`
+    - [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting) via `git`
+- Themes: 
+    - [Powerlevel10k](https://github.com/romkatv/powerlevel10k) via `git`
+
+### Installation
+
+```sh
+sh -c "$(curl -fsSL https://raw.github.com/mondbaron/mdzsh/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# install prerequisites via apt
+sudo apt-get update && sudo apt-get install -yq curl git zsh fonts-powerline
+
+# purge old installations
+rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh}
+rm -f .zsh* .zcompdump* .p10k.zsh
+
+# install oh-my-zsh (unattended)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# add zsh-autosuggestions, zsh-completions and zsh-syntax-highlighting
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# create an inital .zshrc
+cat > ~/.zshrc <<EOF
+export ZSH="\$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+zstyle ':omz:update' mode reminder
+plugins=(git python pip virtualenv zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
+source \$ZSH/oh-my-zsh.sh
+EOF
+
+# lauch the shell to complete setup
+zsh
+
+# re-source final config one more time
+source ~/.zshrc
